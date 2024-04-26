6e841253972d8abacabce0b5a5df3f8ad5945693
Rayen-JN <rayenchuk@gmail.com>
2024-04-26 07:56:14 +0100

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Take two string numbers with an type-annotation concat"
+
+
+def concat(str1: str, str2: str) -> str:
+    "Take two string numbers and returns the concatenated string"
+
+    return str1 + str2
