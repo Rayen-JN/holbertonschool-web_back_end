620c55c06ea8dc081c5fabfe2acc0d5a3bfd8c61
Rayen-JN <rayenchuk@gmail.com>
2024-04-26 07:57:35 +0100

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Take a float n with an type-annotation floor"
+
+
+def floor(n: float) -> int:
+    "Take a float n and returns the floor of the float"
+
+    return int(n)
