815e05aa67b431012e002c4dbfadac284aa2329c
Rayen-JN <rayenchuk@gmail.com>
2024-04-26 07:59:09 +0100

    str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Take a float n with an type-annotation to_str"
+
+
+def to_str(n: float) -> str:
+    "Take a float n and returns the String representation of the float"
+
+    return str(n)
