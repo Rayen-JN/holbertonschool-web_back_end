80d0a30bed61f0b988541874255a5469c8cca011
Rayen-JN <rayenchuk@gmail.com>
2024-04-26 07:53:51 +0100

    read me

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"Sum two float numbers with an type-annotation add"
+
+
+def add(a: float, b: float) -> float:
+    "Take two float numbers and returns the sum"
+
+    return a + b
