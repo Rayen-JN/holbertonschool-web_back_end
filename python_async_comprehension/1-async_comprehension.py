363719c1933f548a8b34ab1f0aa3a0949d792c4f
Rayen-JN <rayenchuk@gmail.com>
2024-04-30 08:38:58 +0100

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-_summary_
+async_comprehension
+:return: list of float
 """
-
 import asyncio
+import random
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """_summary_
-    Async Comprehensions
-    Returns:
-        list[float]: random numbers
     """
-    return [i async for i in async_generator()]
+    async_comprehension
+    :return: list of float
+    """
+    return [_ async for _ in async_generator()]
