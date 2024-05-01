43bf5a94f2c0c63e1e5cd910c79ef4a52863a005
Rayen-JN <rayenchuk@gmail.com>
2024-05-01 18:20:27 +0100

    fix

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python3
-"""
-Python script that provides some stats about Nginx logs stored in MongoDB.
-"""
+"""Logs stats"""
+
 from pymongo import MongoClient
 
 
-def log_infos() -> None:
-    """ Provides some stats about Nginx logs stored in MongoDB. """
-
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
-
-    print(
-        "{} logs".format(
-            nginx.count_documents({})
-        )
-    )
-
-    print("Methods:")
-
-    for method in methods:
-        print(
-            "\tmethod {}: {}".format(
-                method, nginx.count_documents({'method': method})
-            )
-        )
-
-    print(
-        "{} status check".format(
-            nginx.count_documents({'method': 'GET', 'path': '/status'})
-        )
-    )
-
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
 
 if __name__ == "__main__":
-    log_infos()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    print("{} logs".format(nginx_collection.count_documents({})))
+    print("Methods:")
+    get = nginx_collection.count_documents({"method": "GET"})
+    print("\tmethod GET: {}".format(get))
+    post = nginx_collection.count_documents({"method": "POST"})
+    print("\tmethod POST: {}".format(post))
+    put = nginx_collection.count_documents({"method": "PUT"})
+    print("\tmethod PUT: {}".format(put))
+    patch = nginx_collection.count_documents({"method": "PATCH"})
+    print("\tmethod PATCH: {}".format(patch))
+    delete = nginx_collection.count_documents({"method": "DELETE"})
+    print("\tmethod DELETE: {}".format(delete))
+    get_status = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(get_status))
