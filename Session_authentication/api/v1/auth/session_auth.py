b1cb734ba4c16119a52751256b7e76803750fde8
rayen <rayen@kali.kali>
2025-03-08 00:27:15 -1000

    correction session views

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Class to manage the API authentication
+"""
+
+import base64
+
+from .auth import Auth
+from typing import Tuple, TypeVar
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """ Class to manage the API authentication
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id
+    """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID
+    """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value
+    """
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return None
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """deletes the user session / logout:
+    """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
